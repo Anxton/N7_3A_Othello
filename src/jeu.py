7d4eff8d030ece7f514bcfe4a80ac6f878e70a5c
bougnat <96043856+camille4426@users.noreply.github.com>
2026-01-07 14:09:39 +0100

    Update jeu.py

--- src/jeu.py
+++ src/jeu.py
@@ -13,39 +13,86 @@ class Jeu:
         self.colonnes = colonnes
         self.lancer_jeu()
 
-    def lancer_jeu():
+    def lancer_jeu(self):
         #Création plateau
-        self.plateau = [[None for _ in range(colonnes)] for _ in range(lignes)]
-        self.plateau[(colonnes/2)-1][(lignes/2)-1] = "blancs"
-        self.plateau[colonnes/2][lignes/2] = "blancs"
-        self.plateau[colonnes/2][(lignes/2)-1] = "noirs"
-        self.plateau[(colonnes/2)-1][(lignes/2)] = "noirs"
+        self.plateau = [[None for _ in range(self.colonnes)] 
+                        for _ in range(self.lignes)]
+
+        mid_l = self.lignes // 2
+        mid_c = self.colonnes // 2
+        
+        self.plateau[mid_l-1][mid_c-1] = "blancs"
+        self.plateau[mid_l][mid_c] = "blancs"
+        self.plateau[mid_l][mid_c-1] = "noirs"
+        self.plateau[mid_l)-1][mid_c] = "noirs"
       
-        self.tour = blancs
+        self.tour = "blancs"
         interface = OthelloInterface() #front
         interface.initialiserJeu() #METTRE NOM METHODE FRONT
         self.b_peutjouer = True #Indique si les blancs peuvent jouer
         self.n_peutjouer = True #Indique si les noires peuvent jouer
-        self.jouer_tour()
+        self.jouer_tour() #itératif jusqu'à ce qu'aucun joueur ne puisse jouer
+        self.fin_partie()
 
-    def jouer_tour():
+    def coup_valide(self, ligne, colonne):
+        if self.plateau[ligne][colonne] is not None: #case doit etre vide
+            return False
+
+        opposant = 'blancs' if self.tour == 'noirs' else 'noirs'
+        
+        directions = [
+            (-1, -1), (-1, 0), (-1, 1),
+            ( 0, -1),          ( 0, 1),
+            ( 1, -1), ( 1, 0), ( 1, 1)
+        ]
+
+        for dir_l, dir_c in directions:
+            l = ligne + dir_l
+            c = colonne + dir_c
+            trouve_opposant = False
+
+            # On vérifie que l et c sortent pas des possibles
+            while 0 <= l < self.lignes and 0 <= c < self.colonnes:
+                if self.plateau[l][c] == opposant : trouve_opposant = True
+                elif self.plateau[l][c] == self.turn:
+                    if trouve_opposant: return True
+                    else : break
+                else : break #si on trouve Null (ou un ancien coup possible non enlevé encore) on sort
+
+                l += dl
+                c += dc #-> on continue dans la direction tant qu'on a trouve un opposant
+
+        return False #si aucun coup possible n'a été trouvé
+            
+     def jouer_tour(self):
+      peut_jouer = False #si le joueur peut jouer on changera en True
+         
       # Vérification des coups possibles
-      for c in range(0,colonnes)
-        for l in range(0,lignes)
+      for c in range(self.colonnes)
+        for l in range(self.lignes)
             #Supprime les anciens coups possibles
-            if self.plateau[c][l] = "possible" : self.plateau[c][l] = None
+            if self.plateau[l][c] == "possible" : self.plateau[c][l] = None
 
             #Vérifie le tour, les coups possibles, met les nouveaux coups possibles dans plateau
-
+            if self.coup_valide(l,c) :
+                self.plateau[l][c] = "possible"
+                peut_jouer = True
+                
             #Vérifie si le joueur peur jouer, sinon X_peutjouer = False
-
-            #Si oui récupérer coup joueur et fin du tour
+            if peut_jouer is not True : 
+                if self.tour == "blancs" : self.b_peutjouer = False
+                else self.n_peutjouer = False
+                #si peut pas jouer on affiche qu'il ne peut pas jouer avant changement de tour ?
+            else : #Si oui récupérer coup joueur
+                if self.tour == "blancs" : self.b_peutjouer = True
+                else self.n_peutjouer = True
+                new_l, new_c = interface.recuperer_coup() #METTRE NOM METHODE FRONT
+                self.plateau[new_l][new_c] = self.tour
+     
+            # fin du tour
+            if 
 
             #Si joueur peut pas jouer fin du tour, voir comment voir si l'autre peut jouer
-
-      
-      
-      
 
 
 
